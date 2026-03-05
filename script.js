451c930cdb00ffa0f8373c8ba034f4ef141915ae
Fynn <alwaysbuss@gmail.com>
2026-03-06 00:10:08 +0700

    update navbar mobile

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const target = document.querySelector(this.getAttribute('href'));
             if (target) {
                 window.scrollTo({
-                    top: target.offsetTop - 80, // Offset for fixed navbar
+                    top: target.offsetTop - 81, // Offset for fixed navbar
                     behavior: 'smooth'
                 });
             }
